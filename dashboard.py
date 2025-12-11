843d76c755861f25cfeb306745a8cf0d447f8b43
HussainKhanSherwani <ghulamhussainsherwani@gmail.com>
2025-12-12 01:12:11 +0500

    updated dashboard

--- dashboard.py
+++ dashboard.py
@@ -33,9 +33,15 @@ query = """
         current_price,
         sold_last_7_days,
         sold_last_14_days,
+        sold_last_21_days,
         sold_last_30_days,
         total_sold_all_time,
-        item_number
+        item_number,
+        sum(total_sold_all_time) OVER() AS grand_total_sold,
+        sum(sold_last_7_days) OVER() AS grand_total_sold_7_days,
+        sum(sold_last_14_days) OVER() AS grand_total_sold_14_days,
+        sum(sold_last_21_days) OVER() AS grand_total_sold_21_days,
+        sum(sold_last_30_days) OVER() AS grand_total_sold_30_days
     FROM product_performance_view
     ORDER BY sold_last_7_days DESC
 """
@@ -57,12 +63,13 @@ if not df.empty:
         df,
         column_config={
             "image_url": st.column_config.ImageColumn("Image", width="small"),
-            "title": st.column_config.TextColumn("Product Name", width="large"),
+            "title": st.column_config.TextColumn("Product Name", width="medium"),
             "current_price": st.column_config.NumberColumn("Price", format="$%.2f"),
             
             # The Columns you specifically asked for
             "sold_last_7_days": st.column_config.NumberColumn("7 Days", format="%d 📦"),
             "sold_last_14_days": st.column_config.NumberColumn("14 Days", format="%d"),
+            "sold_last_21_days": st.column_config.NumberColumn("21 Days", format="%d"),
             "sold_last_30_days": st.column_config.NumberColumn("30 Days", format="%d"),
             "total_sold_all_time": st.column_config.NumberColumn("Total Sold", format="%d"),
             
